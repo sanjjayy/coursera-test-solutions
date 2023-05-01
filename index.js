79b049d55a2b95d755cfed29ae7239b527bfdb43
URVI BHANU HIRANI <93981701+urvih@users.noreply.github.com>
2023-05-01 10:10:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,28 +8,4 @@
      
      window.byeSpeaker = byeSpeaker;
     })(window);
-(function (window) {  
-    var helloSpeaker = {};
-    var speakWord = "Hello";
-    
-    helloSpeaker.speak = function (names) {
-      console.log(speakWord + " " + names);
-    }
-        window.helloSpeaker = helloSpeaker;
-    })(window);
-(function () {
 
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    
-    for (var i = 0; i < names.length; i++) {
-    
-      var firstLetter = names[i].charAt(0).toLowerCase();
-      if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
-      } 
-      else {
-        helloSpeaker.speak(names[i]);
-      }
-    }
-    
-    })();
